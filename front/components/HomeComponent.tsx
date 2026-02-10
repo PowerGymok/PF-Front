2411ace05c9f02af88dd5831ad106209fdfde86f
jean <nt34161@uvp.edu.mx>
2026-02-10 17:50:33 -0600

    cambio en homecomponent para comprobar PR

--- front/components/HomeComponent.tsx
+++ front/components/HomeComponent.tsx
@@ -4,6 +4,8 @@ export default function HomeComponent() {
     return (
         <div>
             <h1 className="flex justify-center mt-10 text-lg">Hola soy el Home</h1>
+
+            <h2 className="flex justify-center mt-15 text-xl">Prueba de home de PR con equipo</h2>
         </div>
     )
 }
