308e9f2e188c62ea2ae3a8057af8f1f0a5494a1d
MCervantesGonzalez <abg.cervantes@gmail.com>
2026-02-10 14:38:58 -0600

    commit de prueba para flujo de trabajo

--- front/components/HomeComponent.tsx
+++ front/components/HomeComponent.tsx
@@ -1,9 +1,10 @@
-
-
 export default function HomeComponent() {
-    return (
-        <div>
-            <h1 className="flex justify-center mt-10 text-lg">Hola soy el Home</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="flex justify-center mt-10 text-lg">Hola soy el Home</h1>
+      <h1>
+        Soy un texto de prueba, mcervantes probando branches por features{" "}
+      </h1>
+    </div>
+  );
 }
