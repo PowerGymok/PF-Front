14a0ad0947e2a1df528231a6e477d9ce6730fb11
Maria Jose <mjmoretti75@hotmail.com>
2026-02-12 20:03:47 -0300

    chore: config layout global pathroutes navitems

---
+++ front/utils/NavItems.ts
@@ -0,0 +1,25 @@
+import { PATHROUTES } from "./PathRoutes";
+
+export const NavItems = [
+    {
+        id:1,
+        nameToRender: "Home",
+        route: PATHROUTES.HOME
+    },
+    {
+        id:2,
+        nameToRender: "About",
+        route: PATHROUTES.ABOUT
+    },
+    {
+        id:3,
+        nameToRender: "Clases",
+        route: PATHROUTES.CLASES
+    },
+    {
+        id:4,
+        nameToRender: "Registro",
+        route: PATHROUTES.REGISTER
+    },
+   
+]
---
+++ front/utils/PathRoutes.ts
@@ -0,0 +1,8 @@
+export enum PATHROUTES {
+    HOME = "/home",
+    ABOUT = "/about",
+    CLASES = "/clases",
+    REGISTER = "/register",
+    LOGIN = "/login",
+    DASHBOARD ="/dashboard"
+}
