97caa0efe78e877019b637c8ae5159ed891f402f
jean <nt34161@uvp.edu.mx>
2026-02-13 23:08:01 -0600

    Se genero la creacion del mock, la api, y vinculacion sin auth a users

--- front/components/HomeComponent.tsx
+++ front/components/HomeComponent.tsx
@@ -8,7 +8,7 @@ export default function HomeComponent() {
             <div className="relative w-full h-[300px] md:h-[450px] lg:h-[600px]">
                 <Image src="/imagen_fondoPowerGym.jpg" alt="Imagen de fondo" fill className="object-cover rounded-2xl" />
                 <div className="absolute inset-0 flex items-center justify-center top-50">
-                    <Link href="/clases" className="px-4 py-2 text-white mt-4 transition hover:scale-105 md:top[70%]">{<ButtonComponent size="large" label="Reserva Ahora" rounded="lg"  />}</Link>
+                    <Link href="/workshops" className="px-4 py-2 text-white mt-4 transition hover:scale-105 md:top[70%]">{<ButtonComponent size="large" label="Reserva Ahora" rounded="lg"  />}</Link>
                     
                 </div>
                 
