5bdb0e72ceafc740b0a245b64a5fcbdb6e121692
jean <nt34161@uvp.edu.mx>
2026-02-15 00:17:01 -0600

    Persistencia y correccion de errores en AuthContext

--- front/components/LoginComponent.tsx
+++ front/components/LoginComponent.tsx
@@ -4,30 +4,43 @@ import { LoginUserMock } from "@/services/user.services";
 import { useFormik } from "formik";
 import { useRouter } from "next/navigation";
 import InputComponent from "./InputComponent";
+import { useAuth } from "@/app/contexts/AuthContext";
 
 const LoginComponent = () => {
   const router = useRouter();
+  const {setDataUser} = useAuth();
 
   const formik = useFormik({
     initialValues: {
       email: "",
       password: "",
     },
-    onSubmit: async (values) => {
+
+    onSubmit: async (values, {resetForm}) => {
       try {
+
         const user = await LoginUserMock(values);
-        localStorage.setItem("user", JSON.stringify(user));
+        const mockToken = "mock-token";
+
         alert("Inicio de sesión exitoso");
-        console.log("Usuario autenticado:", user);
+        setDataUser({
+            login: true,
+            token: mockToken,
+            user: user,
+        });
+        resetForm()
+
         setTimeout(() => {
           router.push("/home");
         }, 1000);
-        router.refresh();
+
       } catch (error) {
-        alert("Inicio de sesion incorrecto ");
-      }
+        alert("Inicio de sesion incorrecto ");  
+    }
     },
   });
+
+
 
   return (
     <div>
@@ -38,15 +51,26 @@ const LoginComponent = () => {
           placeholder="Correo electrónico"
           value={formik.values.email}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           name="email"
-        />
+        />{
+            formik.errors.email && formik.touched.email && (
+                <div className="text-red-500 text-sm">{formik.errors.email}</div>
+            )
+        }
         <InputComponent
           type="password"
           placeholder="Contraseña"
           value={formik.values.password}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           name="password"
         />
+        {
+            formik.errors.password && formik.touched.password && (
+                <div className="text-red-500 text-sm">{formik.errors.password}</div>
+            )
+        }
         <button type="submit">Iniciar Sesión</button>
       </form>
     </div>
