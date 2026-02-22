7f5cdafa9edf0dd69bd7b2b9c65451afce50edba
Maria Jose <mjmoretti75@hotmail.com>
2026-02-22 18:20:49 -0300

    google-login

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -1,10 +1,11 @@
+import GoogleLoginButton from "@/components/GoogleLoginButton";
 import LoginComponent from "@/components/LoginComponent";
 
 const Login = () => {
     return (
         <div>
             <LoginComponent />
-            
+            <GoogleLoginButton />
         </div>
     ) 
 }
